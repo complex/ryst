e0f16d560ed3fb0d0903d33ef8368c6169e15cd6
Tony Richards <complex@sent.com>
2014-02-12 18:04:09 -0700

    Use a dark theme when in game.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  helper_method :in_game?
+
   def current_location
     request.env['HTTP_EVE_SOLARSYSTEMNAME']
+  end
+
+  def in_game?
+    current_location.present?
   end
 
 end
